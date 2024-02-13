354ddcecf8b756730f823218419c018066492276
nhan <natshi@umich.edu>
2024-02-13 15:07:22 -0500

    Minor alterations

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 
 from config import Config
-from api.model import model
+from app.model import model
 
 
 def create_app():
+    """Create app."""
+    # Initialize app
     app = Flask(__name__)
+    # TODO: deal with config
     app.config.from_object(Config)
 
     if model is None:
         print("Error: no model initialized")
 
-    import api.models
+    import app.models
+    from app import requests
+
+    app.register_blueprint(requests.bp)
 
     return app
---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from gpt2 import *
+from gpt3_5 import *
+from gpt3 import *
+from llama import *
