eb8c827db55125f44dd075e7bdeee32a728a1ea8
nhan <natshi@umich.edu>
2024-03-22 20:30:39 -0400

    Fixed Python interpreter issue in scripts

--- bin/install.sh
+++ bin/install.sh
@@ -3,9 +3,15 @@
 set -Eeuo pipefail
 set -x
 
+if command -v python3 &> /dev/null; then
+    PYTHON=python3
+else 
+    PYTHON=python
+fi
+
 npm install
 
-python3 -m venv env && source env/bin/activate
+$PYTHON -m venv env && source env/bin/activate
 pip install -r requirements.txt
 
 touch app/credentials.py
--- bin/run.sh
+++ bin/run.sh
@@ -3,5 +3,11 @@
 set -Eeuo pipefail
 set -x
 
+if command -v python3 &> /dev/null; then
+    PYTHON=python3
+else 
+    PYTHON=python
+fi
+
 source env/bin/activate
-npm run build & python3 app.py
+npm run build & $PYTHON app.py
