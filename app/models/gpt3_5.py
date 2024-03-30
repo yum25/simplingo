d3f25677e667a5a3dae6c660f29fc7f61d784d2b
nhan <natshi@umich.edu>
2024-03-30 18:26:45 -0400

    German + Polish testing

--- app/models/gpt3_5.py
+++ app/models/gpt3_5.py
@@ -15,20 +15,6 @@ import os
 import time
 from openai import AzureOpenAI
 from app.settings import print_colors as pc
-    
-# client = AzureOpenAI(
-#     api_key=os.getenv("AZURE_OPENAI_API_KEY"),  
-#     api_version="2024-02-01",
-#     azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-#     )
-    
-# deployment_name='REPLACE_WITH_YOUR_DEPLOYMENT_NAME' #This will correspond to the custom name you chose for your deployment when you deployed a model. Use a gpt-35-turbo-instruct deployment. 
-    
-# # Send a completion call to generate an answer
-# print('Sending a test completion job')
-# start_phrase = 'Write a tagline for an ice cream shop. '
-# response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=10)
-# print(start_phrase+response.choices[0].text)
 
 class GPT_35():
     def __init__(self, key, version, endpoint, backup=False):
