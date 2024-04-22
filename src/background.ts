d64d1953f6c7fe6cd4a7b6578ca0e0375dff23d4
nhan <natshi@umich.edu>
2024-04-22 10:59:08 -0400

    URL parameter change

--- src/background.ts
+++ src/background.ts
@@ -15,7 +15,7 @@ const handleMessage = async (type: Message, data: MessageData) => {
   switch (type) {
     case Message.BACKGROUND_REQUEST:
       fetch(
-        `http://127.0.0.1:5000/get_text?translate=${data.translate}&simplify=${data.simplify}&text=${data.text}&target_lang=${data.language}&tagName=${data.tagName}`
+        `http://127.0.0.1:5000/get_text?translate=${data.translate}&simplify=${data.simplify}&target_lang=${data.language}&tagName=${data.tagName}&text=${data.text}`
       )
         .then((resp) => {
           if (!resp.ok) {
